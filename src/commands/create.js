5676c91ffc98dc48a72d51a2ec5f3cdbb5cf212d
Dylan Richardson <dylanrichardson1996@gmail.com>
2018-06-28 15:32:54 -0400

    test(create): Add test and implementation for checking if directory with name exists

--- src/commands/create.js
+++ src/commands/create.js
@@ -1,3 +1,19 @@
 import { command } from 'yargs';
+import { access, constants } from 'fs';
 
-export const create = command('create <name>', 'make a Node.js program');
+const fileExists = path =>
+  new Promise(res => access(path, constants.F_OK, res));
+
+const nameTakenError = () => {
+  throw new Error('File already exists with that name');
+};
+
+/**
+ * TODO docs
+ */
+export const create = name => fileExists(name).then(nameTakenError);
+
+/**
+ * TODO docs
+ */
+export const createCommand = command('create <name>', 'make a Node.js program');
--- test/commands/create.test.js
+++ test/commands/create.test.js
@@ -1,16 +1,26 @@
 import test from 'ava';
-import { create } from '../../src/commands/index';
+import { createCommand, create } from '../../src/commands/index';
 
-test('fails if not given a name', ({ is }) =>
-  create
-    .exitProcess(false)
-    .parse('create', ({ message }) =>
-      is(message, 'Not enough non-option arguments: got 0, need at least 1')
-    ));
+const msgAsync = cmd => (args, expected) => ({ is }) =>
+  cmd(args, ({ message }) => is(message, expected));
 
-test('does not fail if given a name', ({ is }) => {
-  const expected = 'test';
-  create
-    .exitProcess(false)
-    .parse(`create ${expected}`, (_, { name }) => is(name, expected));
-});
+const parseError = msgAsync((args, isCorrect) =>
+  createCommand.exitProcess(false).parse(args, isCorrect)
+);
+
+const createError = msgAsync((name, isCorrect) =>
+  create(name).catch(isCorrect)
+);
+
+test(
+  'fails parse if not given a name',
+  parseError(
+    'create',
+    'Not enough non-option arguments: got 0, need at least 1'
+  )
+);
+
+test(
+  'fails if directory with name exists',
+  createError('test', 'File already exists with that name')
+);
