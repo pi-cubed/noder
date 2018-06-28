cad5dcd8383536da1f25676a5bf276f35123bbb7
Dylan Richardson <dylanrichardson1996@gmail.com>
2018-06-28 13:33:44 -0400

    chore: Add the tests

--- test/commands/create.test.js
+++ test/commands/create.test.js
@@ -1,6 +1,16 @@
 import test from 'ava';
-import Create from '../../src/index';
+import { create } from '../../src/commands/index';
 
-test('foo', ({ pass }) => {
-  pass();
+test('fails if not given a name', ({ is }) =>
+  create
+    .exitProcess(false)
+    .parse('create', ({ message }) =>
+      is(message, 'Not enough non-option arguments: got 0, need at least 1')
+    ));
+
+test('does not fail if given a name', ({ is }) => {
+  const expected = 'test';
+  create
+    .exitProcess(false)
+    .parse(`create ${expected}`, (_, { name }) => is(name, expected));
 });
